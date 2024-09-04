74433ab226a1e5f55e78c40cbe848e305070f0d0
Sophia Wright <wright.a.sophia@gmail.com>
2024-09-04 18:13:16 -0500

    Added test data for properties being missing and added tests to check for them

--- test_receipt_processor_main.py
+++ test_receipt_processor_main.py
@@ -2,11 +2,8 @@ from receipt_processor_main import app
 import re
 import json
 
+# Generate pattern checking for regex provided on IDs
 ID_PATTERN = re.compile("^\\S+$")
-
-with open('data/test_data/simple-receipt.json') as simple_receipt_file:
-    SIMPLE_RECEIPT = json.load(simple_receipt_file)
-
 
 ################################
 # Tests for process_receipts() #
@@ -16,11 +13,13 @@ def test_process_receipts__simple_valid_receipt():
     with app.test_client() as client:
 
         ''' ARRANGE '''
-        # Using simple receipt for data
+        # Using valid simple receipt for data
+        with open('data/test_data/simple-receipt.json') as simple_receipt_file:
+            simple_receipt = json.load(simple_receipt_file)
 
         ''' ACT '''
         # Send the POST request to the API /receipts/process
-        response = client.post('/receipts/process', json=SIMPLE_RECEIPT)
+        response = client.post('/receipts/process', json=simple_receipt)
 
         ''' ASSERT '''
 
@@ -38,3 +37,140 @@ def test_process_receipts__simple_valid_receipt():
 
         # Make sure the 'id' property is in the right format
         assert ID_PATTERN.match(response_data['id'])
+
+def test_process_receipts__morning_valid_receipt():
+    with app.test_client() as client:
+
+        ''' ARRANGE '''
+        # Using valid morning receipt for data
+        with open('data/test_data/morning-receipt.json') as morning_receipt_file:
+            morning_receipt = json.load(morning_receipt_file)
+
+        ''' ACT '''
+        # Send the POST request to the API /receipts/process
+        response = client.post('/receipts/process', json=morning_receipt)
+
+        ''' ASSERT '''
+
+        # Extract the data from the response
+        response_data = response.get_json()
+
+        # Make sure we got a good status code (ie. 200)
+        assert response.status_code == 200, f"{response.data}"
+
+        # Make sure we have an 'id' property
+        assert 'id' in response_data
+
+        # Make sure the 'id' property is a string
+        assert isinstance(response_data['id'], str)
+
+        # Make sure the 'id' property is in the right format
+        assert ID_PATTERN.match(response_data['id'])
+
+def test_process_receipts__invalid_simple_receipt_no_retailer():
+    with app.test_client() as client:
+
+        ''' ARRANGE '''
+        with open('data/test_data/simple-receipt_no_retailer.json') as simple_receipt_no_retailer_file:
+            simple_receipt_no_retailer = json.load(simple_receipt_no_retailer_file)
+
+        ''' ACT '''
+        # Send the POST request to the API /receipts/process
+        response = client.post('/receipts/process', json=simple_receipt_no_retailer)
+
+        ''' ASSERT '''
+        # Make sure we got the correct status code (ie. 404)
+        assert response.status_code == 404, f"{response.data}"
+
+        # Make sure the 'reason the receipt is invalid is for the reason we expect
+        assert response.data == b"Receipt is invalid: 'retailer' is a required property"
+
+def test_process_receipts__invalid_simple_receipt_no_purchaseDate():
+    with app.test_client() as client:
+
+        ''' ARRANGE '''
+        with open('data/test_data/simple-receipt_no_purchaseDate.json') as simple_receipt_no_purchaseDate_file:
+            simple_receipt_no_purchaseDate = json.load(simple_receipt_no_purchaseDate_file)
+
+        ''' ACT '''
+        # Send the POST request to the API /receipts/process
+        response = client.post('/receipts/process', json=simple_receipt_no_purchaseDate)
+
+        ''' ASSERT '''
+        # Make sure we got the correct status code (ie. 404)
+        assert response.status_code == 404, f"{response.data}"
+
+        # Make sure the 'reason the receipt is invalid is for the reason we expect
+        assert response.data == b"Receipt is invalid: 'purchaseDate' is a required property"
+
+def test_process_receipts__invalid_simple_receipt_no_purchaseTime():
+    with app.test_client() as client:
+
+        ''' ARRANGE '''
+        with open('data/test_data/simple-receipt_no_purchaseTime.json') as simple_receipt_no_purchaseTime_file:
+            simple_receipt_no_purchaseTime = json.load(simple_receipt_no_purchaseTime_file)
+
+        ''' ACT '''
+        # Send the POST request to the API /receipts/process
+        response = client.post('/receipts/process', json=simple_receipt_no_purchaseTime)
+
+        ''' ASSERT '''
+        # Make sure we got the correct status code (ie. 404)
+        assert response.status_code == 404, f"{response.data}"
+
+        # Make sure the 'reason the receipt is invalid is for the reason we expect
+        assert response.data == b"Receipt is invalid: 'purchaseTime' is a required property"
+
+def test_process_receipts__invalid_simple_receipt_no_total():
+    with app.test_client() as client:
+
+        ''' ARRANGE '''
+        with open('data/test_data/simple-receipt_no_total.json') as simple_receipt_no_total_file:
+            simple_receipt_no_total = json.load(simple_receipt_no_total_file)
+
+        ''' ACT '''
+        # Send the POST request to the API /receipts/process
+        response = client.post('/receipts/process', json=simple_receipt_no_total)
+
+        ''' ASSERT '''
+        # Make sure we got the correct status code (ie. 404)
+        assert response.status_code == 404, f"{response.data}"
+
+        # Make sure the 'reason the receipt is invalid is for the reason we expect
+        assert response.data == b"Receipt is invalid: 'total' is a required property"
+
+def test_process_receipts__invalid_simple_receipt_no_items():
+    with app.test_client() as client:
+
+        ''' ARRANGE '''
+        with open('data/test_data/simple-receipt_no_items.json') as simple_receipt_no_items_file:
+            simple_receipt_no_items = json.load(simple_receipt_no_items_file)
+
+        ''' ACT '''
+        # Send the POST request to the API /receipts/process
+        response = client.post('/receipts/process', json=simple_receipt_no_items)
+
+        ''' ASSERT '''
+        # Make sure we got the correct status code (ie. 404)
+        assert response.status_code == 404, f"{response.data}"
+
+        # Make sure the 'reason the receipt is invalid is for the reason we expect
+        assert response.data == b"Receipt is invalid: 'items' is a required property"
+
+def test_process_receipts__invalid_simple_receipt_empty_items():
+    with app.test_client() as client:
+
+        ''' ARRANGE '''
+        with open('data/test_data/simple-receipt_empty_items.json') as simple_receipt_empty_items_file:
+            simple_receipt_empty_items = json.load(simple_receipt_empty_items_file)
+
+        ''' ACT '''
+        # Send the POST request to the API /receipts/process
+        response = client.post('/receipts/process', json=simple_receipt_empty_items)
+
+        ''' ASSERT '''
+        # Make sure we got the correct status code (ie. 404)
+        assert response.status_code == 404, f"{response.data}"
+
+        # Make sure the 'reason the receipt is invalid is for the reason we expect
+        assert response.data == b"Receipt is invalid: [] should be non-empty"
